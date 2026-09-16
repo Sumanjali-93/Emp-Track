f70ccf3c55865fc722a6fb368b7ef667a0affb8b
sumanjali <sumanjali1306@gmail.com>
2026-09-16 06:31:21 +0530

    Inital commit-Ems

---
+++ frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const FooterComponent=()=>{
+    return(
+        <div>
+            <footer className="footer">
+                <span>
+                    All right reserved 2023 by java
+                </span>
+
+            </footer>
+            
+        </div>
+    )
+
+}
+export default FooterComponent
